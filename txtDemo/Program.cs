ae2a66c45b6380c0043b2ab3905640daea59e6af
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon-2.local>
2022-12-05 02:16:02 +0100

    sc

---
+++ txtDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine("Hello, World!");
+
+
+var _path = "read";
+
+if (_path == "read")
+{
+    Console.WriteLine("Read");
+    var myLine = System.IO.File.ReadAllText("WriteText.txt");
+    var TagIds = myLine.Split(',').ToList();
+    string url = TagIds[0];
+    string token = TagIds[1];
+
+
+    Console.WriteLine(url);
+    Console.WriteLine(token);
+
+} else
+{
+    Console.WriteLine("Write");
+    // Var
+    string url = "http://master.odooerp.online";
+    string token = "1234";
+
+    // Write in file
+    string text = url + "," + token;
+    await File.WriteAllTextAsync("WriteText.txt", text);
+
+}
+
+Console.ReadLine();
